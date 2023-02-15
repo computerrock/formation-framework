a02f64741b417ebe6e37a633ac37488e81e0d3f8
Ivan Stankovic <ivan.stankovic@webstorm.rs>
2023-02-15 07:06:59 +0100

    add bundler configurations for formation-core & formation-router

---
+++ packages/formation-core/bundler.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    // set additional modules to be processed with webpack
+    sourceModules: [
+        '@computerrock/formation-i18n',
+        '@computerrock/formation-router',
+    ],
+};
--- packages/formation-router/bundler.config.js
+++ packages/formation-router/bundler.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     // set additional source modules to be processed with Storybook's webpack
     sourceModules: [
+        '@computerrock/formation-i18n',
         '@computerrock/formation-ui',
     ],
 };
