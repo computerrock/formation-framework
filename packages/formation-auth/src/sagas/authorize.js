b54548f4181d5b94a18ba607024fb29378ff8aea
Luka-Krsmanovic <luka.krsmanovic@computerrock.com>
2024-06-04 15:24:02 +0200

    acelea-2955 extend auth scope with phone

--- packages/formation-auth/src/sagas/authorize.js
+++ packages/formation-auth/src/sagas/authorize.js
@@ -9,7 +9,7 @@ const authorize = function* authorize(redirectURI) {
     const {authServiceName} = this;
     const {serviceManager} = yield select(state => state.application);
     const authService = serviceManager.loadService(authServiceName);
-    const authorizationEndpoint = authService.getAuthorizationEndpoint({responseType: 'code', redirectURI, scope: 'openid profile email'});
+    const authorizationEndpoint = authService.getAuthorizationEndpoint({responseType: 'code', redirectURI, scope: 'openid profile email phone'});
     yield call(() => window.location.assign(authorizationEndpoint));
 };
 
