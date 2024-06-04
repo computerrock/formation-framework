d3ef903483c5a58f6eaa8f55a9404165134c5ba4
Luka-Krsmanovic <luka.krsmanovic@computerrock.com>
2024-06-04 16:02:55 +0200

    acelea-2955 [misc] add a comment

--- packages/formation-auth/src/sagas/authorize.js
+++ packages/formation-auth/src/sagas/authorize.js
@@ -9,6 +9,7 @@ const authorize = function* authorize(redirectURI) {
     const {authServiceName} = this;
     const {serviceManager} = yield select(state => state.application);
     const authService = serviceManager.loadService(authServiceName);
+    // extend authorization scope with phone, see https://computerrock.atlassian.net/browse/ACELEA-2945
     const authorizationEndpoint = authService.getAuthorizationEndpoint({responseType: 'code', redirectURI, scope: 'openid profile email phone'});
     yield call(() => window.location.assign(authorizationEndpoint));
 };
